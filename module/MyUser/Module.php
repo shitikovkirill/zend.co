68337a5a478f86dfeeffb541d1a2f2ec97931e11
shitikovkirill <sh.kiruh@gmail.com>
2015-12-23 21:52:26 +0200

    Admin + Role

---
+++ module/MyUser/Module.php
@@ -0,0 +1,33 @@
+<?php
+namespace MyUser;
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function onBootstrap($mvcEvent)
+    {
+        $zfcServiceEvents = $mvcEvent->getApplication()->getServiceManager()->get('zfcuser_user_service')->getEventManager();
+        $zfcServiceEvents->attach('register', function($e) use($mvcEvent) {
+            $user = $e->getParam('user');
+            $em = $mvcEvent->getApplication()->getServiceManager()->get('doctrine.entitymanager.orm_default');
+            $config = $mvcEvent->getApplication()->getServiceManager()->get('config');
+            $defaultUserRole = $em->getRepository('ZfcUserCrud\Entity\Role')->findOneBy(array('roleId' => $config['bjyauthorize']['authenticated_role']));
+            $user->addRole($defaultUserRole);
+        });
+    }
+}
