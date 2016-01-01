31c1d074bcfea845d3cbd01e2e0001592a6705ac
shitikovkirill <sh.kiruh@gmail.com>
2016-01-01 20:49:20 +0200

    User

--- config/autoload/zfcuser.global.php
+++ config/autoload/zfcuser.global.php
@@ -40,7 +40,7 @@ $settings = array(
      *
      * Accepted values: boolean true or false
      */
-    //'enable_username' => false,
+    'enable_username' => true,
 
     /**     
      * Authentication Adapters
@@ -60,7 +60,7 @@ $settings = array(
      *
      * Accepted values: boolean true or false
      */
-    //'enable_display_name' => true,
+    'enable_display_name' => true,
 
     /**
      * Modes for authentication identity match
@@ -71,7 +71,7 @@ $settings = array(
      * Default value: array containing 'email'
      * Accepted values: array containing one or more of: email, username
      */
-    //'auth_identity_fields' => array( 'email' ),
+    'auth_identity_fields' => array( 'email', 'username'),
 
     /**
      * Login form timeout
